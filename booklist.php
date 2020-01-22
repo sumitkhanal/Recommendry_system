1ffd650804a60c7395966f58cfb1cbb3cc0e70be
sumitkhanal <55523893+sumitkhanal@users.noreply.github.com>
2020-01-22 14:07:23 +0545

    Add files via upload

---
+++ booklist.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Table with database</title>
+<style>
+table {
+border-collapse: collapse;
+width: 100%;
+color: #588c7e;
+font-family: monospace;
+font-size: 14px;
+text-align: left;
+}
+th {
+background-color: #588c7e;
+color: white;
+}
+tr:nth-child(even) {background-color: #f2f2f2}
+</style>
+</head>
+<body>
+<table>
+<tr>
+<th>AUTHOR</th>
+<th>TITLE</th>
+<th> images</th>
+<th>GENRE</th>
+</tr>
+<?php
+$conn = mysqli_connect("localhost", "root", "", "csv_db");
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);
+}
+$sql = "SELECT FILENAME, TITLE,CATEGORY FROM table2";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+// output data of each row
+while($row = $result->fetch_assoc()) {
+echo "<tr><td>" . $row["FILENAME"]. "</td><td>" . $row["TITLE"] . "</td><td>"
+. $row["CATEGORY"]. "</td></tr>";
+}
+echo "</table>";
+} else { echo "0 results"; }
+$conn->close();
+?>
+</table>
+</body>
+</html>
